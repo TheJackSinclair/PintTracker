68d9956849552d034f8d8cf930a148ae30d060c4
cammyearly <2762365e@student.gla.ac.uk>
2024-04-05 18:13:57 +0100

    allowed user input from form to generate a basic user .json file

--- api/index.py
+++ api/index.py
@@ -1,5 +1,33 @@
-from flask import Flask
+from dataclasses import dataclass
+
+from flask import Flask, request
+import json
+import os
+from datetime import datetime
+
 app = Flask(__name__)
+
+@dataclass
+class Account:
+    def __init__(self, username, password, email, time_of_request):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.time_of_request = time_of_request
+
+
+@app.route("/api/account/create", methods=["POST"])
+def create_account():
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
+    email = data["email"]
+    account = Account(username, password, email, datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    with open(os.path.join("api/accounts", f"{account.username}.json"), "w") as f:
+        json.dump(account.__dict__, f, indent=4)
+    return "<p>Account created</p>"
+
+
 
 @app.route("/api/python")
 def hello_world():
