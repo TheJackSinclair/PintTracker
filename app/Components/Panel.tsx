ab7ffaf59279350511e14375f223f418eadc3fd2
BuildTools <unconfigured@null.spigotmc.org>
2024-04-06 12:39:38 +0100

    35: Allowed colour changing

--- app/Components/Panel.tsx
+++ app/Components/Panel.tsx
@@ -1,6 +1,7 @@
 interface PanelProps {
     children: React.ReactNode;
     width: 'small' | 'medium' | 'large'
+    shadow: 'yellow' | 'orange'
 }
 
 const Panel = (props: PanelProps) => {
@@ -11,7 +12,12 @@ const Panel = (props: PanelProps) => {
         large: 'lg:w-3/4',
     };
 
-    const panelClassName = `grid bg-pt-offwhite rounded-[30px] p-8 border-6 border-pt-brown min-h-[30rem] ${sizeClasses[props.width]} shadow-3xl shadow-pt-yellow`;
+    const shadowColour = {
+        yellow: 'shadow-pt-yellow',
+        orange: 'shadow-pt-orange'
+    }
+
+    const panelClassName = `grid bg-pt-offwhite rounded-[30px] p-8 border-6 border-pt-brown min-h-[30rem] ${sizeClasses[props.width]} shadow-3xl ${shadowColour[props.shadow]}`;
 
     return (
             <div className={panelClassName}>
