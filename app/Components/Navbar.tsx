70a4a10e8a7aeec0468ccc93852f3f8544ade8cd
Jack Sinclair <2668867s@student.gla.ac.uk>
2024-04-06 12:03:54 +0100

    33: Removed un-need code and files, gave custom beige pt identifier, updated...

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 const NavBar = () => {
     return (
-        <nav className=" bg-[#FFD93D]">
+        <nav className="bg-pt-yellow drop-shadow-md">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-left mx-auto p-3">
                 <a href="/homepage">
-                    <Image src={'/navbar.svg'} alt={'Pintlogo'} height={175} width={175}/>
+                    <Image src={'/lightLogo.svg'} alt={'Pintlogo'} height={175} width={175} className={'drop-shadow-lg'}/>
                 </a>
 
             </div>
