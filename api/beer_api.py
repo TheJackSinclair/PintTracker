2ab5532258f89908f8957fba93b21119ca975eed
cammyearly <2762365e@student.gla.ac.uk>
2024-04-09 14:29:25 +0100

    cached global beer stats for easy access

--- api/beer_api.py
+++ api/beer_api.py
@@ -1,5 +1,12 @@
+import json
+import os
+
 import pandas as pd
 from unidecode import unidecode
+
+# File path for global stats
+global_stats_file = 'global_drank_stats.json'
+directory = "stats"
 
 def normalize(text):
     return unidecode(text.lower()).replace("'","")  # "normalizes" text by converting to lower case, removing accents, and ignoring apostrophes
@@ -22,3 +29,26 @@ def search_data(query):
                 results.append(result)
                 break
     return results[:10]
+
+
+def load_global_stats():
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    if not os.path.exists(os.path.join(directory, global_stats_file)):
+        return {}
+    with open(os.path.join(directory, global_stats_file), 'r') as f:
+        return json.load(f)
+
+
+def save_global_pint_stats(stats):
+    with open(os.path.join(directory, global_stats_file), 'w') as f:
+        json.dump(stats, f, indent=4)
+
+
+def increment_pint_stat(beer_name):
+    stats = load_global_stats()
+    if beer_name in stats:
+        stats[beer_name] += 1
+    else:
+        stats[beer_name] = 1
+    save_global_pint_stats(stats)
