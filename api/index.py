1b41bb7c6112c721544b345a38213a8808c28f1c
finlayM04 <1177frys@gmail.com>
2024-04-09 21:15:46 +0100

    tidied index.py, created Pint.py, Account.py and createAccount.py

--- api/index.py
+++ api/index.py
@@ -1,73 +1,14 @@
 from flask import Flask, request
+from api import beer_api, createAccount
 import json
-import os
-from datetime import datetime
-import beer_api
-import accountFinder
-import toDict
 
 app = Flask(__name__)
 
 
-class Account:
-    def __init__(self, username, password, email, member_since):
-        self.username = username
-        self.password = password
-        self.email = email
-        self.member_since = member_since
-        self.added = []
-        self.added_you = []
-        self.pint_history = {}
-
-    def add_user(self, username):
-        if accountFinder.user_exists(username):
-            self.added.append(username)
-            friend = accountFinder.get_user(username)
-            friend.added_you.append(self.username)
-            friend_path = accountFinder.get_path(username)
-            with open(friend_path, 'w') as file:
-                dict_entry = toDict.to_dict(friend)
-                json.dump(dict_entry, file, indent=4)
-
-
-    def add_pint(self, pint):
-        # Use the pint's timestamp as the key
-        self.pint_history[pint.timestamp] = pint.__dict__
-
-
-class Pint:
-    def __init__(self, timestamp, name, full_name, abv, style):
-        self.timestamp = timestamp
-        self.name = name
-        self.full_name = full_name
-        self.abv = abv
-        self.style = style
-
-
 @app.route("/api/account/create", methods=["POST"])
-def create_account():
+def create():
     data = request.get_json()
-    username = data["username"]
-    password = data["password"]
-    email = data["email"]
-    account = Account(username, password, email, datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-    added = []
-    added_you = []
-
-    def add_user(self, username):
-        if accountFinder.user_exists(username):
-            self.added.append(username)
-            friend = accountFinder.get_user(username)
-            friend.added_you.append(self.username)
-
-    directory = "api/accounts"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    with open(os.path.join(directory, f"{account.username}.json"), "w") as f:
-        json.dump(account.__dict__, f, indent=4)
-
-    return "<p>Account created</p>"
+    createAccount.create_account(data)
 
 
 @app.route("/api/beer/search/<query>")
