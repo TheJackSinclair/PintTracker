f60935beab51e44f09aa8ce0fed233fb7cc6152d
cammyearly <2762365e@student.gla.ac.uk>
2024-04-10 11:32:47 +0100

    added most_drank_day stat

--- api/userAnalytics.py
+++ api/userAnalytics.py
@@ -4,6 +4,32 @@ def pints_drank(user_pints) -> int:
     # returns the amount of total drank pints
     return len(user_pints)
 
+def most_drank_day(user_pints):
+    if len(user_pints) == 0:
+        return None
+
+    day_counter = {}
+
+    for key in user_pints.keys():
+        # converts pint to dictionary
+        pint = vars(user_pints[key])
+
+        # gets the pint date
+        pint_date = pint['timestamp'][:10]
+
+        # check the frequency of the pint date and adds it to the counter
+        if pint_date in day_counter:
+            day_counter[pint_date] += 1
+        else:
+            day_counter[pint_date] = 1
+
+    # returns the most drank day, along with its frequency
+    day = max(day_counter, key=day_counter.get)
+    most_drank_day_frequency = day_counter[day]
+
+    return {'day': day, 'pints_drank': most_drank_day_frequency}
+
+
 
 def favourite_pint(user_pints) -> str:
     pint_counter = {}
@@ -51,5 +77,7 @@ def get_analytics(user_profile):
     user_stats['favourite_pint'] = favourite_pint(user_pints)
 
     user_stats['strongest_pint_drank'] = strongest_pint_in_pint_history(user_pints)
+
+    user_stats['most_drank_day'] = most_drank_day(user_pints)
 
     return user_stats
