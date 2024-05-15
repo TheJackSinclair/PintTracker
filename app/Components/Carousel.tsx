77928e7dbab50b65ef04ae3631f3a0f18c6f8ec9
Jack <thejacksinclair4@gmail.com>
2024-05-15 10:28:01 +0100

    MASTER: Deployment Fixes

--- app/Components/Carousel.tsx
+++ app/Components/Carousel.tsx
@@ -1,32 +1,15 @@
-{/* Carousel component for a users statistics. Usage: <Carousel> wasup playa </Carousel> */}
-
-import React, { useRef, useState, useEffect } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper } from 'swiper/react';
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
-import { fetchUsername } from '../Common/UserCommon'
 
 interface CarouselProps {
   children: React.ReactNode;
 }
 
 export default function Carousel(props: CarouselProps) {
-    const [username, setUsername] = useState<string | null>(null);
-
-    // this is like onMounted() in vuejs
-    useEffect(() => {
-        let usernameFromLocalStorage = fetchUsername();
-        // Update the state only if the name is not null
-        if (usernameFromLocalStorage !== null) {
-            setUsername(usernameFromLocalStorage);
-        }
-    }, []);
-
-    
-
   return (
     <>
       <Swiper
